0c946d276b834ce33dc3541de4bcccb036b330cb
Chewie <none@none>
2010-03-26 07:46:43 +0100

    [Chwthewke.PasswordManager2] * Migration: Importer Tests

--- Chwthewke.PasswordManager.Test/Migration/LegacyItemImporterTest.cs
+++ Chwthewke.PasswordManager.Test/Migration/LegacyItemImporterTest.cs
@@ -1,6 +1,13 @@
-using Chwthewke.PasswordManager.Editor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Security;
+using Chwthewke.PasswordManager.Engine;
 using Chwthewke.PasswordManager.Migration;
 using Chwthewke.PasswordManager.Storage;
+using Chwthewke.PasswordManager.Test.Engine;
+using Chwthewke.PasswordManager.Test.Storage;
 using Moq;
 using NUnit.Framework;
 
@@ -22,7 +29,85 @@ namespace Chwthewke.PasswordManager.Test.Migration
 
             _serializerMock = new Mock<IPasswordStoreSerializer>( );
 
-            _importer = new LegacyItemImporter( _passwordStoreMock.Object, _passwordDigesterMock.Object, _serializerMock.Object );
+            _importer = new LegacyItemImporter( _passwordStoreMock.Object, _passwordDigesterMock.Object,
+                                                _serializerMock.Object );
+        }
+
+        [ Test ]
+        public void ImportDigestsItems( )
+        {
+            // Setup
+            IEnumerable<LegacyItem> legacyItems = new[ ]
+                                                      {
+                                                          new LegacyItem( "aKey", false ),
+                                                          new LegacyItem( "anotherKey", true ),
+                                                      };
+            SecureString masterPassword = Util.Secure( "p@ssw" );
+
+            // Exercise
+            _importer.Import( legacyItems, masterPassword );
+            // Verify
+            _passwordDigesterMock.Verify( DoDigest( "aKey", masterPassword, PasswordGenerators.Full ) );
+            _passwordDigesterMock.Verify( DoDigest( "anotherKey", masterPassword, PasswordGenerators.AlphaNumeric ) );
+        }
+
+        [ Test ]
+        public void ImportAddsDigestsToStore( )
+        {
+            // Setup
+            SecureString masterPassword = Util.Secure( "p@ssw" );
+
+            PasswordDigest passwordDigest1 = new PasswordDigestBuilder( ).WithKey( "aKey" );
+            _passwordDigesterMock.Setup( DoDigest( "aKey", masterPassword, PasswordGenerators.Full ) )
+                .Returns( passwordDigest1 );
+            PasswordDigest passwordDigest2 = new PasswordDigestBuilder( ).WithKey( "anotherKey" );
+            _passwordDigesterMock.Setup( DoDigest( "anotherKey", masterPassword, PasswordGenerators.AlphaNumeric ) )
+                .Returns( passwordDigest2 );
+
+            IEnumerable<LegacyItem> legacyItems = new[ ]
+                                                      {
+                                                          new LegacyItem( "aKey", false ),
+                                                          new LegacyItem( "anotherKey", true ),
+                                                      };
+
+            // Exercise
+            _importer.Import( legacyItems, masterPassword );
+            // Verify
+            _passwordStoreMock.Verify( s => s.AddOrUpdate( passwordDigest1 ) );
+            _passwordStoreMock.Verify( s => s.AddOrUpdate( passwordDigest2 ) );
+        }
+
+        [ Test ]
+        public void SaveCallsSerializer( )
+        {
+            // Setup
+
+            try
+            {
+                // Exercise
+                _importer.Save( "__tmp__" );
+                // Verify
+                _serializerMock.Verify( s => s.Save( _passwordStoreMock.Object,
+                                                     It.Is<FileStream>( fs => fs.Name.EndsWith( @"\__tmp__" ) ) ) );
+            }
+            finally
+            {
+                // Teardown
+                File.Delete( "__tmp__" );
+            }
+        }
+
+
+        private static Expression<Func<IPasswordDigester, PasswordDigest>> DoDigest( string akey,
+                                                                                     SecureString masterPassword,
+                                                                                     IPasswordGenerator
+                                                                                         passwordGenerator )
+        {
+            return d => d.Digest( akey,
+                                  passwordGenerator.MakePassword( akey, masterPassword ),
+                                  It.IsAny<Guid>( ),
+                                  passwordGenerator.Id,
+                                  It.IsAny<string>( ) );
         }
     }
 }

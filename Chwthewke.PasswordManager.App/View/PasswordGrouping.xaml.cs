8bbb690640a172b66e546bf902199c5b9426aadc
Chewie <none@none>
2010-03-31 08:40:35 +0200

    [Chwthewke.PasswordManager2] * Exploratory UI Design

---
+++ Chwthewke.PasswordManager.App/View/PasswordGrouping.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Chwthewke.PasswordManager.App.ViewModel;
+
+namespace Chwthewke.PasswordManager.App.View
+{
+    /// <summary>
+    /// Interaction logic for PasswordGrouping.xaml
+    /// </summary>
+    public partial class PasswordGrouping
+    {
+        public PasswordGrouping( )
+        {
+            InitializeComponent( );
+        }
+
+        public PasswordGroupingViewModel ViewModel
+        {
+            get { return DataContext as PasswordGroupingViewModel; }
+            set { DataContext = value; }
+        }
+    }
+}

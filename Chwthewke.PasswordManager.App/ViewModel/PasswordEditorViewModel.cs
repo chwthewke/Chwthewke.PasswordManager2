e383395e3ed7af8a5f1e00bfe2ba4b3d1ffe0fd9
Chewie <none@none>
2010-04-09 08:46:49 +0200

    [Chwthewke.PasswordManager2] * Interface sketch with Pencil * Working on the ViewModel tests

--- Chwthewke.PasswordManager.App/ViewModel/PasswordEditorViewModel.cs
+++ Chwthewke.PasswordManager.App/ViewModel/PasswordEditorViewModel.cs
@@ -1,7 +1,39 @@
+using System;
+using Chwthewke.MvvmUtils;
+
 namespace Chwthewke.PasswordManager.App.ViewModel
 {
-    public class PasswordEditorViewModel
+    public class PasswordEditorViewModel : ObservableObject
     {
-        
+        public string Key
+        {
+            get { return _key; }
+            set
+            {
+                if ( _key == value )
+                    return;
+                _key = value;
+                RaisePropertyChanged( ( ) => Key );
+                Title = Key + "*";
+            }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if ( _title == value )
+                    return;
+                _title = value;
+                RaisePropertyChanged( ( ) => Title );
+            }
+        }
+
+
+
+        private string _key;
+        private string _title;
+
     }
 }
--- Chwthewke.PasswordManager.Test/App/PasswordEditorViewModelTest.cs
+++ Chwthewke.PasswordManager.Test/App/PasswordEditorViewModelTest.cs
@@ -1,7 +1,38 @@
+using Chwthewke.PasswordManager.App.ViewModel;
+using NUnit.Framework;
+
 namespace Chwthewke.PasswordManager.Test.App
 {
+    [TestFixture]
     public class PasswordEditorViewModelTest
     {
-        
+        private PasswordEditorViewModel _viewModel;
+
+        [SetUp]
+        public void SetUpPasswordEditorViewModel( )
+        {
+            _viewModel = new PasswordEditorViewModel( );
+        }
+
+        [ Test, Ignore ]
+        public void InitialStateIsVoid( )
+        {
+            // Setup
+            // Exercise
+            // Verify
+            Assert.That( _viewModel.Key, Is.EqualTo( string.Empty ) );
+            Assert.That( _viewModel.Title, Is.EqualTo( "(new)" ) );
+            // Assert.That commands.CanExecute( ) is false... 
+        }
+
+        [ Test ]
+        public void TabHeaderUpdatedByKeyUpdates( )
+        {
+            // Setup
+            // Exercise
+            _viewModel.Key = "abc";
+            // Verify
+            Assert.That( _viewModel.Title, Is.EqualTo( "abc*" ) );
+        }
     }
 }

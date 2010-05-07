53c83891d7592f520dcc5b7affc28d452d99108b
chewie <none@none>
2010-05-07 07:55:47 +0200

    [Chwthewke.PasswordManager2]
    * Tests for PasswordList

--- Chwthewke.PasswordManager.Test/App/ViewModel/PasswordListTest.cs
+++ Chwthewke.PasswordManager.Test/App/ViewModel/PasswordListTest.cs
@@ -14,13 +14,16 @@ namespace Chwthewke.PasswordManager.Test.App.ViewModel
     public class PasswordListTest
     {
         [ SetUp ]
-        public void SetUpPasswordList( )
+        public void SetUpContainer( )
         {
             ContainerBuilder containerBuilder = new ContainerBuilder( );
             containerBuilder.RegisterModule( new PasswordManagerModule( ) );
             containerBuilder.RegisterType<ClipboardService>( ).As<IClipboardService>( );
             containerBuilder.RegisterType<PasswordEditorFactory>( ).As<IPasswordEditorFactory>( );
             _container = containerBuilder.Build( );
+
+            _passwordList = new PasswordListViewModel( _container.Resolve<IPasswordStore>( ),
+                                                       _container.Resolve<IPasswordEditorFactory>( ) );
         }
 
         [ Test ]
@@ -35,7 +38,78 @@ namespace Chwthewke.PasswordManager.Test.App.ViewModel
                                                        _container.Resolve<IPasswordEditorFactory>( ) );
             // Verify
             Assert.That( _passwordList.Items.Select( x => x.Name ).ToArray( ),
-                Is.EqualTo( new[ ] { "abc", "abcd", "abde" } ) );
+                         Is.EqualTo( new[ ] { "abc", "abcd", "abde" } ) );
+        }
+
+        [ Test ]
+        public void AddEmptyEditorToList( )
+        {
+            // Setup
+            // Exercise
+            _passwordList.OpenEditorCommand.Execute( null );
+            // Verify
+            Assert.That( _passwordList.Editors.Count, Is.EqualTo( 1 ) );
+            Assert.That( _passwordList.Editors[ 0 ].Key, Is.EqualTo( string.Empty ) );
+            Assert.That( _passwordList.Editors[ 0 ].IsKeyReadonly, Is.False );
+        }
+
+        [ Test ]
+        public void LoadPasswordIntoNewEditor( )
+        {
+            // Setup
+            _container.AddPassword( "abc", string.Empty, PasswordGenerators.Full, "123".ToSecureString( ) );
+            // Exercise
+            _passwordList.OpenNewEditor( "abc" );
+            // Verify
+            Assert.That( _passwordList.Editors.Count, Is.EqualTo( 1 ) );
+            Assert.That( _passwordList.Editors[ 0 ].Key, Is.EqualTo( "abc" ) );
+            Assert.That( _passwordList.Editors[ 0 ].IsKeyReadonly, Is.True );
+        }
+
+        [ Test ]
+        public void PasswordListIsUpdatedByEditorChange( )
+        {
+            // Setup
+            _passwordList.OpenEditorCommand.Execute( null );
+            var editor = _passwordList.Editors[ 0 ];
+            // Exercise
+            editor.Key = "abcd";
+            editor.UpdateMasterPassword( "1234".ToSecureString( ) );
+            editor.Slots[ 0 ].IsSelected = true;
+            editor.SaveCommand.Execute( null );
+            // Verify
+            var store = _container.Resolve<IPasswordStore>( );
+            Assert.That( store.Passwords.Count( ), Is.EqualTo( 1 ) );
+            Assert.That( _passwordList.Items.Select( it => it.Name ).ToArray( ), Is.EqualTo( new[ ] { "abcd" } ) );
+        }
+
+        [ Test ]
+        public void EditorIsClosedByItsRequest( )
+        {
+            // Setup
+            _passwordList.OpenEditorCommand.Execute( null );
+            // Exercise
+            _passwordList.Editors[ 0 ].CloseCommand.Execute( null );
+            // Verify
+            Assert.That( _passwordList.Editors, Is.Empty );
+        }
+
+        [Test]
+        public void PasswordListIsNoLongerUpdatedByClosedEditorChange( )
+        {
+            // Setup
+            _passwordList.OpenEditorCommand.Execute( null );
+            var editor = _passwordList.Editors[ 0 ];
+            // Exercise
+            editor.CloseCommand.Execute( null );
+            editor.Key = "abcd";
+            editor.UpdateMasterPassword( "1234".ToSecureString( ) );
+            editor.Slots[ 0 ].IsSelected = true;
+            editor.SaveCommand.Execute( null );
+            // Verify
+            var store = _container.Resolve<IPasswordStore>( );
+            Assert.That( store.Passwords.Count( ), Is.EqualTo( 1 ) );
+            Assert.That( _passwordList.Items.Select( it => it.Name ), Is.Empty );
         }
 
         private IContainer _container;

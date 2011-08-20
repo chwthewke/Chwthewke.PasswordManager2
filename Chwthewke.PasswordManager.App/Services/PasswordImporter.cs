438d0add8cd7911281f1784ff8cba16bfd8e7dc5
Chewie@C-3PO.home <Chewie@C-3PO.home>
2011-08-20 21:28:07 +0200

    [Chwthewke.PasswordManager2] * Password import merges MasterPasswordId

--- Chwthewke.PasswordManager.App/Services/PasswordImporter.cs
+++ Chwthewke.PasswordManager.App/Services/PasswordImporter.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using Chwthewke.PasswordManager.Storage;
+using System.Linq;
 
 namespace Chwthewke.PasswordManager.App.Services
 {
@@ -15,17 +17,39 @@ namespace Chwthewke.PasswordManager.App.Services
         // TODO possibly return a "report" to be presented to the user
         public void ImportPasswords( FileInfo externalPasswordFile )
         {
-            IEnumerable<PasswordDigest> passwords = _passwordSerializer.Load( new FilePasswordStore( externalPasswordFile ) );
+            IEnumerable<PasswordDigest> passwords = _passwordSerializer.Load( new FilePasswordStore( externalPasswordFile ) ).ToList( );
 
-
+            IDictionary<Guid, Guid> masterPasswordRelations = new Dictionary<Guid, Guid>( );
+            IList<PasswordDigest> toImport = new List<PasswordDigest>( );
             foreach ( PasswordDigest passwordDigest in passwords )
             {
-                if ( _passwordDatabase.FindByKey( passwordDigest.Key ) == null )
+                PasswordDigest currentDigest = _passwordDatabase.FindByKey( passwordDigest.Key );
+                if ( currentDigest == null )
+                    toImport.Add( passwordDigest );
+                else if ( currentDigest.Hash.SequenceEqual( passwordDigest.Hash ) )
+                    masterPasswordRelations[ passwordDigest.MasterPasswordId ] = currentDigest.MasterPasswordId;
+            }
+
+            foreach ( PasswordDigest passwordDigest in toImport )
+            {
+                if ( !masterPasswordRelations.ContainsKey( passwordDigest.MasterPasswordId ) )
                     _passwordDatabase.AddOrUpdate( passwordDigest );
+                else
+                {
+                    PasswordDigest fixedCopy = new PasswordDigest( passwordDigest.Key,
+                                                                   passwordDigest.Hash,
+                                                                   masterPasswordRelations[ passwordDigest.MasterPasswordId ],
+                                                                   passwordDigest.PasswordGeneratorId,
+                                                                   passwordDigest.CreationTime,
+                                                                   passwordDigest.ModificationTime,
+                                                                   passwordDigest.Note );
+                    _passwordDatabase.AddOrUpdate( fixedCopy );
+                }
             }
         }
 
         private readonly IPasswordSerializer _passwordSerializer;
         private readonly IPasswordDatabase _passwordDatabase;
+        private readonly IMasterPasswordMatcher _masterPasswordMatcher;
     }
 }

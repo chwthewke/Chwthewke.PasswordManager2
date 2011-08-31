2b981f38b868c0393e3a1e41cb7cd224952d0747
tdufour@TDUFOUR.ankama.lan <tdufour@TDUFOUR.ankama.lan>
2011-08-31 14:01:02 +0200

    [Chwthewke.PasswordManager2] * (wip) Improve password tooltip in list.

---
+++ Chwthewke.PasswordManager.App/ViewModel/PasswordGeneratorNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Chwthewke.PasswordManager.Engine;
+
+namespace Chwthewke.PasswordManager.App.ViewModel
+{
+    class PasswordGeneratorNames
+    {
+        public static string GeneratorName( IPasswordGenerator generator )
+        {
+            return GeneratorNames[ generator.Id ];
+        }
+
+        public static string GeneratorName( Guid generatorId )
+        {
+            return GeneratorNames[ generatorId ];
+        }
+
+        private static readonly IDictionary<Guid, string> GeneratorNames =
+            new Dictionary<Guid, string>( )
+                {
+                    { PasswordGenerators.AlphaNumeric.Id, "Alpha-numeric" },
+                    { PasswordGenerators.Full.Id, "Complex"}
+                };
+    }
+}
--- Chwthewke.PasswordManager.App/ViewModel/StoredPasswordViewModel.cs
+++ Chwthewke.PasswordManager.App/ViewModel/StoredPasswordViewModel.cs
@@ -12,6 +12,16 @@ namespace Chwthewke.PasswordManager.App.ViewModel
             get { return _name; }
         }
 
+        public string Note
+        {
+            get { return _note; }
+        }
+
+        public bool NoteVisible
+        {
+            get { return !string.IsNullOrWhiteSpace( _note ); }
+        }
+
         public Guid MasterPasswordGuid
         {
             get { return _masterPasswordGuid; }
@@ -32,19 +42,28 @@ namespace Chwthewke.PasswordManager.App.ViewModel
             get { return _modificationDate; }
         }
 
+        public string GeneratorName
+        {
+            get { return _generatorName; }
+        }
+
         public StoredPasswordViewModel(PasswordDigest password, Color masterPasswordColor)
         {
             _name = password.Key;
+            _note = password.Note;
             _masterPasswordGuid = password.MasterPasswordId;
             _masterPasswordColor = masterPasswordColor;
             _creationDate = password.CreationTime.ToString( );
             _modificationDate = password.ModificationTime.ToString( );
+            _generatorName = PasswordGeneratorNames.GeneratorName( password.PasswordGeneratorId );
         }
 
         private readonly string _name;
+        private readonly string _note;
         private readonly Guid _masterPasswordGuid;
         private readonly Color _masterPasswordColor;
         private readonly string _creationDate;
         private readonly string _modificationDate;
+        private readonly string _generatorName;
     }
 }

b77ec88e5dfb262e0f739cdd5effc77a21e27a1f
Chewie@Ebon-Hawk <Chewie@Ebon-Hawk>
2011-11-17 11:44:34 +0100

    [Chwthewke.PasswordManager2] * Custom exception for file access errors

--- Chwthewke.PasswordManager.Storage/FileTextResource.cs
+++ Chwthewke.PasswordManager.Storage/FileTextResource.cs
@@ -1,9 +1,29 @@
 using System;
 using System.IO;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Chwthewke.PasswordManager.Storage
 {
+    public class PasswordsFileException: Exception
+    {
+        public PasswordsFileException( )
+        {
+        }
+
+        public PasswordsFileException( string message ) : base( message )
+        {
+        }
+
+        public PasswordsFileException( string message, Exception innerException ) : base( message, innerException )
+        {
+        }
+
+        protected PasswordsFileException( SerializationInfo info, StreamingContext context ) : base( info, context )
+        {
+        }
+    }
+
     public class FileTextResource : ITextResource
     {
         public FileTextResource( FileInfo passwordsFile )
@@ -13,15 +33,46 @@ namespace Chwthewke.PasswordManager.Storage
             _passwordsFile = passwordsFile;
         }
 
-
         public TextReader OpenReader( )
         {
-            return new StreamReader( _passwordsFile.Open( FileMode.OpenOrCreate, FileAccess.Read ), new UTF8Encoding( false ) );
+            return WrappingFileExceptions( () => 
+                new StreamReader( _passwordsFile.Open( FileMode.OpenOrCreate, FileAccess.Read ), new UTF8Encoding( false ) ) );
         }
 
         public TextWriter OpenWriter( )
         {
-            return new StreamWriter( _passwordsFile.Open( FileMode.Create, FileAccess.Write ), new UTF8Encoding( false ) );
+            return WrappingFileExceptions( () => 
+                new StreamWriter( _passwordsFile.Open( FileMode.Create, FileAccess.Write ), new UTF8Encoding( false ) ) );
+        }
+
+
+        private A WrappingFileExceptions<A>( Func<A> function )
+        {
+            try
+            {
+                return function();
+            }
+            catch ( ArgumentException e )
+            {
+                throw passwordsFileException( e );
+            }
+            catch ( UnauthorizedAccessException e )
+            {
+                throw passwordsFileException( e );
+            }
+            catch ( DirectoryNotFoundException e )
+            {
+                throw passwordsFileException( e );
+            }
+            catch ( IOException e )
+            {
+                throw passwordsFileException( e );
+            }
+        }
+
+        private Exception passwordsFileException( Exception innerException )
+        {
+            throw new PasswordsFileException( "A file error occured", innerException );
         }
 
         private readonly FileInfo _passwordsFile;

53da89a2feab5b1771b4aa64da2acbd6fa871229
Chewie@C-3PO.home <Chewie@C-3PO.home>
2011-11-25 08:27:55 +0100

    [Chwthewke.PasswordManager2]  * (wip) sketching delayed execution of update

---
+++ Chwthewke.PasswordManager.App/Services/DelayedExecutor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chwthewke.PasswordManager.App.Services
+{
+    public class DelayedExecutor
+    {
+        private volatile int _sequenceNumber;
+
+        public void Execute( Action<Func<bool>> cancelableAction )
+        {
+            _sequenceNumber++;
+        }
+    }
+
+}
